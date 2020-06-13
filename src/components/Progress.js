f08a4bb2c1bcc43ac5d63db4d3616000b0b8b63a
Brian Archibald <brianallenarchibald@gmail.com>
2020-06-13 13:29:53 -0500

    removed unused files, css done, made progress component

---
+++ src/components/Progress.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Progress = () => (
+    <div className="progress">
+        <div className="progress-done" data-done="70">
+            70%
+        </div>
+    </div>
+)
+
+export default Progress;
