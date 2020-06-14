62e89a569bb1df8e2f154eb1395b87143b82c068
Brian Archibald <brianallenarchibald@gmail.com>
2020-06-14 14:33:19 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Progress from '../components/Progress';
+import Progress from './components/Progress';
 import './App.css';
 
 const App = () => (
   <>
     <h1>React Progress Bar</h1>
-    <Progress />
+    <Progress done="70" />
+    <Progress done="50" />
+    <Progress done="30" />
+    <Progress done="100" />
   </>
 )
 
--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
-const Progress = () => (
+const Progress = ({ done }) => {
+    const [style, setStyle] = React.useState({});
+
+    setTimeout(() => {
+        const newStyle = {
+            opacity: 1,
+            width: `${done}%`
+        }
+
+        setStyle(newStyle);
+    }, 1000);
+
+    return (
     <div className="progress">
-        <div className="progress-done" data-done="70">
-            70%
+        <div className="progress-done" style={style}>
+            {done}%
         </div>
     </div>
-)
+    )
+}
 
 export default Progress;
